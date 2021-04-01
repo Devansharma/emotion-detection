667a90f9a320d7292545a2b985ff1efc70ef1d0c
Devansh Sharma <devansharma92@gmail.com>
2021-04-01 18:47:39 +0530

    created the trainning module

--- preprocess.py
+++ preprocess.py
@@ -2,7 +2,7 @@ import pandas as pd
 import cv2
 import numpy as np
 
-path = 'Data\fer2013\fer2013.csv'
+path = r'D:\Projects\emotion-detection\Data\fer2013\fer2013.csv'
 image_size = (48,48)
 
 def load_data():
@@ -10,14 +10,14 @@ def load_data():
     pixels = data['pixels'].tolist()
     width, height = 48, 48
     faces = []
-    for pixel_seq in pixels:
-        face = [int(pixel) for pixel in pixel_seq.split(' ')]
+    for pixel_sequence in pixels:
+        face = [int(pixel) for pixel in pixel_sequence.split(' ')]
         face = np.asarray(face).reshape(width, height)
-        face = cv2.resize(face.astype('uint8'), image_size)
+        face = cv2.resize(face.astype('uint8'),image_size)
         faces.append(face.astype('float32'))
     faces = np.asarray(faces)
     faces = np.expand_dims(faces, -1)
-    emotions = pd.get_dummies(data['emotion']).as_matrix()
+    emotions = pd.get_dummies(data['emotion']).values
     return faces, emotions
 
 def preprocess_input(x, v2=True):

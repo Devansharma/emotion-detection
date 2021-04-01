70df0bc9eed9ffea4c0692af04774dc409f75f15
Devansh Sharma <devansharma92@gmail.com>
2021-04-01 17:22:17 +0530

    Create a simple CNN model

--- models/cnn.py
+++ models/cnn.py
@@ -9,3 +9,37 @@ from keras.layers import MaxPooling2D
 from keras.layers import SeparableConv2D
 from keras import layers
 from keras.regularizers import l2
+
+def simple_CNN(input_shape, num_classes):
+    model = Sequential()
+    model.add(Convolution2D(filters=16, kernel_size=(7, 7), padding='same',
+                            name='image_array', input_shape=input_shape))
+    model.add(BatchNormalization())
+    model.add(Convolution2D(filters=16, kernel_size=(7,7), padding='same'))
+    model.add(BatchNormalization())
+    model.add(Activation('relu'))
+    model.add(AveragePooling2D(pool_size=(2,2), padding='same'))
+    model.add(Dropout(0.5))
+
+    model.add(Convolution2D(filters=64, kernel_size=(3, 3), padding='same',))
+    model.add(BatchNormalization())
+    model.add(Convolution2D(filters=64, kernel_size=(3,3), padding='same'))
+    model.add(BatchNormalization())
+    model.add(Activation('relu'))
+    model.add(AveragePooling2D(pool_size=(2,2), padding='same'))
+    model.add(Dropout(0.5))
+
+    model.add(Convolution2D(filters=128, kernel_size=(3, 3), padding='same',))
+    model.add(BatchNormalization())
+    model.add(Convolution2D(filters=128, kernel_size=(3,3), padding='same'))
+    model.add(BatchNormalization())
+    model.add(Activation('relu'))
+    model.add(AveragePooling2D(pool_size=(2,2), padding='same'))
+    model.add(Dropout(0.5))
+
+    model.add(Convolution2D(filters=256, kernel_size=(3, 3), padding='same',))
+    model.add(BatchNormalization())
+    model.add(Convolution2D(filters=num_classes, kernel_size=(3,3), padding='same'))
+    model.add(GlobalAveragePooling2D())
+    model.add(Activation('softmax', name='Predictions'))
+    return model
